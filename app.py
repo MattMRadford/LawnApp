5fa6c3b30a82d4fdf4a0dc3eca154521ffe916c0
Matthew Radford <radfom@rpi.edu>
2025-05-18 18:25:40 -0400

    adding app.py

--- app.py
+++ app.py
@@ -8,4 +8,29 @@ def get_db_connection():
     connection.row_factory = sqlite3.Row
     return connection
 
+#Home Page
+@app.route('/')
+def index():
+    connection = get_db_connection()
+    clients = connection.execute('SELECT * FROM clients').fetchall()
+    connection.close()
+    return render_template('index.html', clients=clients)
 
+@app.route('/add_client', methods=['GET', 'POST'])
+def add_client():
+    if request.method == 'POST':
+        name = request.form['name']
+        address = request.form['address']
+        phone = request.form['phone']
+        frequency = request.form['frequency']
+
+        connection = get_db_connection()
+        connection.execute('INSERT INTO clients (name, address, phone, frequency) VALUES (?, ?, ?, ?)', name, address, phone, frequency)
+        connection.commit()
+        connection.close()
+        return redirect(url_for('index'))
+    return render_template('add_client.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
